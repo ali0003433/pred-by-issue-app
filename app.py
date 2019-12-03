3271af32501280d8fcbd273664f6fe2125f77f16
Alyssa Liguori <alyssa.liguori@gmail.com>
2019-12-03 13:47:19 -0500

    Add html edits for form

--- app.py
+++ app.py
@@ -1,57 +1,36 @@
 import flask
-import cgi, cgitb
 from flask import request, jsonify, render_template
 from funcs import make_prediction
 
 # initialize app 
 app = flask.Flask(__name__)
 
-@app.route('/', methods=['GET'])
-def index():
-    return render_template('index.html')
-
 @app.route('/', methods=['POST'])
-def predict():
-    form = cgi.FieldStorage()
-    # res_sz
-    if form.getvalue('size'):
-        res_sz = form.getvalue('size')
+def survey_page():
+    if request.method == 'POST': 
+        print(request.form)
+        print('printing post')
+        res_size = request.form.get('size')
+        res_racial = request.form.get('racial')
+        res_climate = request.form.get('climate')
+        res_budget = request.form.get('budget')
+        res_immigration = request.form.get('immigration')
+        res_terrorism = request.form.get('terrorism')
+        res_gender = request.form.get('gender')
+        print('res_size:', res_size, 'res_racial:', res_racial, 'res_climate:', res_climate, 'res_terrorism:', res_terrorism, 'res_budget:', res_budget, 'res_immigration:', res_immigration, 'res_gender:', res_gender)
+        return 'submitted form'
     else:
-        res_sz = '2'
-    # res_rc
-    if form.getvalue('racial'):
-        res_rc = form.getvalue('racial')
-    else:
-        res_rc = '8'
-    #res_clm
-    if form.getvalue('climate'):
-        res_clm = form.getvalue('climate')
-    else:
-        res_clm = '8'
-    # res_bgt
-    if form.getvalue('budget'):
-        res_bgt = form.getvalue('budget')
-    else:
-        res_bgt = '8'
-    # res_imm
-    if form.getvalue('immigration'):
-        res_imm = form.getvalue('immigration')
-    else: 
-        res_imm = '8'
-    # res_trr
-    if form.getvalue('terrorism'):
-        res_trr = form.getvalue('terrorism')
-    else:
-        res_trr = '8'
-    # res_gdr
-    if form.getvalue('gender'):
-        res_gdr = form.getvalue('gender')
-    else:
-        res_gdr = '8'
-    res_dict = {'res_sz': res_sz, 'res_rc': res_rc, 'res_clm': res_clm, 'res_bgt': res_bgt, 'res_imm': res_imm, 'res_trr': res_trr, 'res_gdr': res_gdr}
-    print(res_dict)
-    return render_template('index.html')
+        return flask.render_template('index.html')
 
+@app.route('/', methods=['GET'])
+def render_page():
+    if request.args:
+        print(request.args)
+        prediction = make_prediction()
+        print(prediction)
+        return flask.render_template('res.html')
+    else: 
+        return flask.render_template('index.html')
 # start the server, listen for requests 
 if __name__ == '__main__':
     app.run(debug=True)  # local dev
--- funcs.py
+++ funcs.py
@@ -9,16 +9,7 @@ import random
 from sklearn.linear_model import LogisticRegression
 
 def make_prediction(res_dict):
-    # import model
-    clf = joblib.load('./clf.pkl')
-    return clf
-    # convert form into dataframe
-    # clean form dataframe 
-    # new_form_clnd = FUNC(new_form)
-    # pred_class = clf.predict(new_form_clnd[0])
-    # print('Predicted class:', pred_class)
-    # if pred_class == 1:
-    #     label_to_return = 'Russian Troll'
-    # else:
-    #     label_to_return = 'Not a Russian Troll'
-    # return label_to_return                          
+    # clf = joblib.load('./clf.pkl')
+    # return clf
+    print('running prediction')
+    return 'ran prediction'  
